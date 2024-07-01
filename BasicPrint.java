6714e7d2ce94dbe2d0b00ec4250ad0aad724ff1c
Radhey <radheyanpatil@gmail.com>
2024-07-01 20:13:23 +0530

    first commit

---
+++ BasicPrint.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class BasicPrint{
+   
+     
+     
+        
+      
+
+    
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         System.out.println("===print m1()===");
+        System.out.println("value of a ="+a);
+       
+        System.out.println(" ===print m2()====");
+        System.out.println("value of a="+a);
+        System.out.println("value of b="+b);
+        sc.close();
+        
+
+     
+
+    }
+}
